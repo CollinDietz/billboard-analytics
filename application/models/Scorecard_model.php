90f278aa35caa702af683112d025beda0649990f
Collin Dietz <c4dietz@gmail.com>
2018-11-13 22:09:30 -0500

    formatted sql

--- application/models/Scorecard_model.php
+++ application/models/Scorecard_model.php
@@ -10,7 +10,12 @@ class ScoreCard_model extends CI_Model {
 
   function GetChartEntriesStats($artist_id)
   {
-    $PerChartStats = 'SELECT chart_name, count(date), count(distinct date) FROM (SELECT date, chart_id FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ? UNION All SELECT date, chart_id FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a NATURAL JOIN CHARTS GROUP BY chart_name';
+    $PerChartStats =
+    'SELECT chart_name, count(date), count(distinct date) FROM
+      (SELECT date, chart_id FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ?
+        UNION All
+       SELECT date, chart_id FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a
+       NATURAL JOIN CHARTS GROUP BY chart_name';
     $results = $this->db->query($PerChartStats, array($artist_id,$artist_id));
     $data = $results->result_array();
     $chart_entry_pairs = [];
@@ -24,7 +29,11 @@ class ScoreCard_model extends CI_Model {
 
   function GetAllTimeStats($artist_id)
   {
-    $AllTimeDateStats = 'SELECT count(date), count(distinct date) FROM (SELECT date FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ? UNION ALL SELECT date FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a';
+    $AllTimeDateStats =
+    'SELECT count(date), count(distinct date) FROM
+    (SELECT date FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ?
+      UNION ALL
+     SELECT date FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a';
     $results = $this->db->query($AllTimeDateStats, array($artist_id,$artist_id));
     $results = $results->result_array()[0];
     $RenamedResults = array(
@@ -36,14 +45,23 @@ class ScoreCard_model extends CI_Model {
 
   function FirstAndLastApperances($artist_id)
   {
-    $FirstAndLastApperancePerChart = 'SELECT chart_name, min(date) as FirstApperance, max(date) as MostRecentApperance FROM (SELECT date, chart_id FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ? UNION ALL SELECT date, chart_id FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a NATURAL JOIN CHARTS GROUP by chart_id order by min(date) ASC';
+    $FirstAndLastApperancePerChart =
+    'SELECT chart_name, min(date) as FirstApperance, max(date) as MostRecentApperance
+     FROM (SELECT date, chart_id FROM CHARTED NATURAL JOIN SONGS WHERE artist_id = ?
+        UNION ALL SELECT date, chart_id FROM CHARTED NATURAL JOIN ALBUMS WHERE artist_id = ?)a
+     NATURAL JOIN CHARTS GROUP by chart_id order by min(date) ASC';
     $results = $this->db->query($FirstAndLastApperancePerChart, array($artist_id,$artist_id));
     return $results->result_array();
   }
 
   function ArtistsSongsFirstAndLastApperances($artist_id)
   {
-    $FirstAndLastApperancePerChart = 'SELECT song_name, chart_name, min(date) as FirstAppearance, max(date) as MostRecentApperance from SONGS NATURAL JOIN CHARTED NATURAL JOIN CHARTS WHERE artist_id = ? GROUP BY song_name, chart_name HAVING min(date)';
+    $FirstAndLastApperancePerChart =
+    'SELECT song_name, chart_name,
+    min(date) as FirstAppearance,
+    max(date) as MostRecentApperance
+    FROM SONGS NATURAL JOIN CHARTED NATURAL JOIN CHARTS
+    WHERE artist_id = ? GROUP BY song_name, chart_name HAVING min(date)';
     $results = $this->db->query($FirstAndLastApperancePerChart, array($artist_id));
     $results = $results->result_array();
     return $results;
